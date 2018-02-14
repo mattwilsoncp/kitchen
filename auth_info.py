c8b416c62a803c44b6db5ac984c56ea23beee718
Matt Wilson <mattwilsoncp16@gmail.com>
2018-02-13 21:30:19 -0500

    Nightly commit

--- auth_info.py
+++ auth_info.py
@@ -1,6 +1,7 @@
 from requests import request, ConnectionError
 from django.http import HttpResponse, Http404
 from .models import Profile
+from django.shortcuts import redirect
 
 
 def save_profile(user, response, *args, **kwargs):
@@ -8,4 +9,9 @@ def save_profile(user, response, *args, **kwargs):
     url = response['image'].get('url')
     user.profile.avatar_url = url
     user.save()
-    
+
+def redirect_if_no_refresh_token(backend, response, social, *args, **kwargs):
+    if backend.name == 'google-oauth2' and social and \
+       response.get('refresh_token') is None and \
+       social.extra_data.get('refresh_token') is None:
+        return redirect('/login/google-oauth2?approval_prompt=force')
